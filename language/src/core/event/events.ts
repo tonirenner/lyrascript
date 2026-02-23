9a2b95590c69e5638978e0434da0206e20f42e38
Toni Renner <mail.toni.renner@gmail.com>
2026-02-23 21:04:01 +0100

    formatting

--- language/src/core/event/events.ts
+++ language/src/core/event/events.ts
@@ -1,7 +1,6 @@
-const LyraEvents =
-	{
-		LYRA_INSTANCE_DIRTY_STATE: 'lyra:instance_dirty_state',
-		LYRA_INSTANCE_CLEAN_STATE: 'lyra:instance_clean_state'
-	}
+const LyraEvents = {
+	LYRA_INSTANCE_DIRTY_STATE: 'lyra:instance_dirty_state',
+	LYRA_INSTANCE_CLEAN_STATE: 'lyra:instance_clean_state'
+};
 
 export default LyraEvents;
